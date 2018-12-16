0a7663e1c26c85df373db8b75151649a4b5f1268
Sameeh <sameehkhan1@gmail.com>
2018-12-15 23:29:22 -0800

    Re-style instapx logo

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -16,8 +16,8 @@ class NavBar extends React.Component {
         return( 
             <div className="main-nav">
                     <button onClick={this.handleLogout}>Log Out</button>
-                    <span><img className="ig-logo" src='/assets/ig-logo.png'/></span>
-                    <div><h2 id='logo'>Instapx</h2></div>
+                    <img className="ig-logo" src='/assets/ig-logo.png'/>
+                    <h2 id='nav-logo'>Instapx</h2>
                     <div className='search-bar'><input type="text" placeholder="Search.."/></div>
             </div>
         );
