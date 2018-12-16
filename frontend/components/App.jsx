1f686bd462f127f91d9d078a64aa0924f0c85e1d
Sameeh <sameehkhan1@gmail.com>
2018-12-16 11:40:39 -0800

    Add feed container to app presentational component

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -4,12 +4,14 @@ import LoginFormContainer from './session/login_form_container';
 import SignUpFormContainer from './session/signup_form_container';
 import UserIndexContainer from './user/user_index_container';
 import NavBarContainer from './nav_bar/nav_bar_container';
+import FeedContainer from './feed/feed_container';
  import { AuthRoute, ProtectedRoute } from '../util/route_utils';
 
 
 const App = () => (
     <div className='big-div'>
             <ProtectedRoute path='/' component={NavBarContainer}/>
+            <ProtectedRoute path='/feed' component={FeedContainer}/>
             <Switch>
                 <AuthRoute exact path="/login" component={LoginFormContainer}/>
                 <AuthRoute exact path="/" component={SignUpFormContainer}/>
