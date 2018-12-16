069966a96d9d5c8307f50b9f11a6e67be715615b
Sameeh <sameehkhan1@gmail.com>
2018-12-15 23:46:24 -0800

    Search input sizing

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -18,7 +18,7 @@ class NavBar extends React.Component {
                     <button onClick={this.handleLogout}>Log Out</button>
                     <img className="ig-logo" src='/assets/ig-logo.png'/>
                     <div className='nav-logo'><h2 id='logo-words'>Instapx</h2></div>
-                    <div className='search-bar'><input type="text" placeholder="Search.."/></div>
+                    <div className='search-bar'><input className='search-input'type="text" placeholder="Search.."/></div>
             </div>
         );
     }
