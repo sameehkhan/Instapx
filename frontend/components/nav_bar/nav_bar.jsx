950b0b426f67f4814c6bc2c049e61e15e316a68f
Sameeh <sameehkhan1@gmail.com>
2018-12-17 21:13:43 -0800

    Nav bar logo edits

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -17,8 +17,10 @@ class NavBar extends React.Component {
         return( 
             <div className="main-nav">
                     <button onClick={this.handleLogout}>Log Out</button>
-                    <img className="ig-logo" src='/assets/ig-logo.png'/>
-                    <div className='nav-logo'><h2 id='logo-words'>Instapx</h2></div>
+                    {/* <img className="ig-logo" src='/assets/ig-logo.png'/> */}
+                    <img className="nav-bar-word" src='/assets/instapxlogo.png' />
+                    <img className="nav-bar-line" src='/assets/logo-line.png' />
+                    <img className="nav-bar-logo" src='/assets/instapx.png'/>
                     <div className='search-bar'><input className='search-input'type="text" placeholder="Search"/></div>
                     <Link to={`/user`} id='login-button'>Sign up</Link>
             </div>
