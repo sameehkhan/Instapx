9ef0596dc2236dd5feea6f3bff4df5284132deb0
Sameeh <sameehkhan1@gmail.com>
2018-12-17 22:05:07 -0800

    Turn logos into links to feed and user profile

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -20,9 +20,15 @@ class NavBar extends React.Component {
                     {/* <button onClick={this.handleLogout}>Log Out</button> */}
                     {/* <img className="ig-logo" src='/assets/ig-logo.png'/> */}
                     <div className='all-nav-logos'>
-                    <img className="nav-bar-logo" src='/assets/instapxlogo.png' />
+                    <Link to={`/feed`}>
+                            <img className="nav-bar-logo" src='/assets/instapxlogo.png' />
+                    </Link>
+
                     <img className="nav-bar-line" src='/assets/logo-line.png' />
-                    <img className="nav-bar-word" src='/assets/instapx.png'/>
+                    
+                    <Link to={`/feed`}>
+                        <img className="nav-bar-word" src='/assets/instapx.png'/>
+                    </Link>
                     </div>
                     <div className='search-bar'><input className='search-input'type="text" placeholder="Search"/></div>
                     <div className='user-icon'><Link to={`/user`}><img className="prof-icon" src='/assets/profile-icon.png' /></Link></div>
