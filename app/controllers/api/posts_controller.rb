bfbe687bec8b83bc4e5d2d76d0476c76ce532bbb
Sameeh <sameehkhan1@gmail.com>
2018-12-18 10:04:02 -0800

    Update posts controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,11 +1,33 @@
 class Api::PostsController < ApplicationController
 
+def index
+    @users = User.all
+    @posts = Post.all    
+    render :index
+end
+
+def create
+    @post = Post.new(post_params)
+    @post = current_user.id
+    if @post.save 
+        render :show 
+    else 
+        render json: @post.errors.full_messages, status: 422
+    end 
+end 
+
 def show 
     @post = Post.find(params[:id])
     render :show
 end 
 
+def destroy
+    @post = Post.find(params[:id])
+    @post.delete! if @post
+    render :show    
+end
+
 def post_params
-    params.require(:caption).permit(:title, photos: [])
+    params.require(:post).permit(:caption, :photo, :image_url)
 end
 end
