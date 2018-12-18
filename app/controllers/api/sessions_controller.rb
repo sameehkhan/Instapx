2ea3df47364b469c9c7fd45e1775a47c2c5a62ba
Sameeh <sameehkhan1@gmail.com>
2018-12-18 13:23:58 -0800

    Change render routes in session and users controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,7 +3,7 @@ class Api::SessionsController < ApplicationController
     @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
     if @user
       login(@user)
-      render "api/user/show"
+      render "api/users/show"
     else
       render json: ["Invalid credentials"], status: 401
     end
--- frontend/actions/post_actions.js
+++ frontend/actions/post_actions.js
@@ -14,8 +14,6 @@ export const receivePosts = ({ posts, users }) => {
     };
 };
 
-
-
 export const receivePost = (post) => {
     return {
         type: RECEIVE_POST,
