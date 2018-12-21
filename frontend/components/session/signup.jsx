e37f1f74cc654102c239fdfdfdad486573a16b59
Sameeh <sameehkhan1@gmail.com>
2018-12-20 18:23:35 -0800

    Position display iphone

--- frontend/components/session/signup.jsx
+++ frontend/components/session/signup.jsx
@@ -43,7 +43,7 @@ class SignupForm extends React.Component {
         return (
             <div className="outer-div">
                 <div className="parent-image">
-                <img id="temp" src={window.iphone} />
+                <img id="iphone" src={window.iphone} />
                 <img id="paris" src={window.paris} />
                 <img id="temp" src={window.temp} />
                 </div>
