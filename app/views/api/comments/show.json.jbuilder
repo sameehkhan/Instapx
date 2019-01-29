fd3e4a8003c3aaad8f1206abeb12972ee5db67d4
Sameeh <sameehkhan1@gmail.com>
2019-01-29 10:52:18 -0800

    Update post reducer to include comments

--- app/views/api/comments/show.json.jbuilder
+++ app/views/api/comments/show.json.jbuilder
@@ -1,4 +1,4 @@
-json.extract! @comment, :id, :body, :user_id, :image_id
+json.extract! @comment, :id, :body, :user_id, :post_id
 
 json.username @comment.user.username
 json.receiver_id @comment.receiver.id
--- frontend/reducers/posts_reducer.js
+++ frontend/reducers/posts_reducer.js
@@ -2,6 +2,10 @@ import { RECEIVE_POSTS, RECEIVE_POST, REMOVE_POST } from '../actions/post_action
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 import { RECEIVE_USER } from '../actions/user_actions';
 
+import { RECEIVE_COMMENT, REMOVE_COMMENT} from '../actions/comment_actions';
+
+
+
 import merge from 'lodash/merge';
 
 const postsReducer = (state = {}, action) => {
@@ -18,6 +22,17 @@ const postsReducer = (state = {}, action) => {
         case RECEIVE_CURRENT_USER:
         case RECEIVE_USER:
             return merge({}, state, action.posts);
+        case RECEIVE_COMMENT:
+            if (newState[action.comment.postId].comments) {
+                newState[action.comment.postId].comments[action.comment.id] = action.comment;
+            } else if (newState[action.comment.postId]) {
+                newState[action.comment.postId].comments = {};
+                newState[action.comment.postId].comments[action.comment.id] = action.comment;
+            }
+            return newState;
+        case REMOVE_COMMENT:
+            delete newState[action.comment.postId].comments[action.comment.id];
+            return newState;
         default:
             return state;
     }
