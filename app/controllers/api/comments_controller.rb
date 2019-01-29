f3514c179196623a4ea0603fed841a3d2fda76b9
Sameeh <sameehkhan1@gmail.com>
2019-01-29 10:24:52 -0800

    Finish comments controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,23 @@
 class Api::CommentsController < ApplicationController
+  def index
+    @comments = Image.where(post_id: params[:id])
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save
+    render :show
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    render :show
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:post_id, :body)
+  end
 end
