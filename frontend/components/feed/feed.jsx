9e30ed41d8b9521417b3f5d07f18f8bcacad1964
Sameeh <sameehkhan1@gmail.com>
2019-01-29 15:39:36 -0800

    Link username/profile picture to user profile

--- frontend/components/feed/feed.jsx
+++ frontend/components/feed/feed.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CommentContainer from '../comment/comment_container';
+import { Link } from 'react-router-dom';
 
 class Feed extends React.Component {
     constructor(props) {
@@ -27,8 +28,9 @@ class Feed extends React.Component {
                     return (
                         <div key={post.id} className='feed-post' tabIndex="0">
                             <div className='post-bar'>
-                                <div className='canvas'><img className='post-prof-pic' src={post.user_photo} /> </div>
-                                <div><h2 className='post-username'>{post.username}</h2></div> 
+                                <div className='canvas'><Link to={`/users/${post.user_id}`}><img className='post-prof-pic' src={post.user_photo} /> </Link></div>
+                                <div><h2 className='post-username'><Link to={`/users/${post.user_id}`}>{post.username}</Link></h2></div> 
+                                
                             </div>
                             <img className='post-index-img' src={post.photo_url} />
                             <div className='comment-bar'>
