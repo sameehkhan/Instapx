205be98d531c27f3d4aa400a0070f3a2dd331c10
Sameeh <sameehkhan1@gmail.com>
2019-01-29 10:44:39 -0800

    Create comment api util

---
+++ frontend/util/comment_api_util.js
@@ -0,0 +1,22 @@
+export const createComment = comment => (
+    $.ajax({
+        method: 'POST',
+        url: 'api/comments',
+        data: { comment }
+    })
+);
+
+export const fetchComments = postId => (
+    $.ajax({
+        method: 'GET',
+        url: 'api/comments',
+        data: { postId }
+    })
+);
+
+export const deleteComment = commentId => (
+    $.ajax({
+        method: 'DELETE',
+        url: `api/comments/${commentId}`
+    })
+);
