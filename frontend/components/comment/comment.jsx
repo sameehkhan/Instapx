bc1379064a9a375706ee25dce37f8480feaf7dd3
Sameeh <sameehkhan1@gmail.com>
2019-01-30 13:23:12 -0800

    Add update field function for comment

--- frontend/components/comment/comment.jsx
+++ frontend/components/comment/comment.jsx
@@ -4,13 +4,19 @@ import React from 'react';
 class Comment extends React.Component {
     constructor(props) {
         super(props);
-        this.state = Object.assign({}, this.props.comment)
+        this.state = Object.assign({}, this.props.comment);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.update = this.update.bind(this);
 
     }
 
-    componentDidMount() {
-        this.props.fetchComments();
+    update(field) {
+        return e => this.setState({
+            [field]: e.target.value
+        })
     }
+
 
     render() {
         return (
