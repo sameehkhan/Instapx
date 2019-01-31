d4470e03bbcd07e4455a4660756be651a26acdab
Sameeh <sameehkhan1@gmail.com>
2019-01-31 14:58:07 -0800

    Add comments off of the post modal

--- frontend/components/comment/comment.jsx
+++ frontend/components/comment/comment.jsx
@@ -23,26 +23,15 @@ class Comment extends React.Component {
         e.preventDefault();
         console.log(this.state)
         this.props.action(this.state).then( ()=> this.setState({body: ''}));
-            // .then(() => {
-            //     this.props.history.push("/feed")
-            // });
     }
 
-    // onKeyDown = (event: React.KeyboardEvent<HTMLDivElement>): void => {
-    //     // 'keypress' event misbehaves on mobile so we track 'Enter' key via 'keydown' event
-    //     if (event.key === 'Enter') {
-    //         event.preventDefault();
-    //         event.stopPropagation();
+
+    // handleKeyPress(e) {
+    //     if (e.key == 'Enter') {
+    //         e.preventDefault();
     //         this.onSubmit();
     //     }
     // }
-
-    handleKeyPress(e) {
-        if (e.key == 'Enter') {
-            e.preventDefault();
-            this.onSubmit();
-        }
-    }
 
     render() {
         // debugger
@@ -50,7 +39,7 @@ class Comment extends React.Component {
             <div className='comment-creator-container'>
                 <div className='create-comment-wrapper'>
                     <form onSubmit={this.handleSubmit}>
-                        <textarea onKeyDown={this.handleKeyPress} className="create-post-comment" placeholder="Add a comment..." value={this.state.body} onChange={this.update("body")} />
+                        <input type='text' className="create-post-comment" placeholder="Add a comment..." value={this.state.body} onChange={this.update("body")} />
                         <input className='hidden-submit' type="submit" value="Submit"/>
                     </form>
                 </div>
