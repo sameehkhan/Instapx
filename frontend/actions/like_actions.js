db1bad8f30735ebe48e1a0a88d5a3782b1c22332
Sameeh <sameehkhan1@gmail.com>
2019-02-01 11:55:49 -0800

    Implement like actions

---
+++ frontend/actions/like_actions.js
@@ -0,0 +1,24 @@
+import * as LikeAPIUtil from '../util/like_api_util';
+
+export const RECEIVE_LIKE = "RECEIVE_LIKE";
+export const REMOVE_LIKE = "REMOVE_LIKE";
+
+const receiveLike = like => ({
+    type: RECEIVE_LIKE,
+    like: like
+});
+
+const removeLike = like => ({
+    type: REMOVE_LIKE,
+    like: like
+});
+
+export const createLike = like => dispatch => (
+    LikeAPIUtil.createLike(like)
+        .then(like => dispatch(receiveLike(like)))
+);
+
+export const deleteLike = imageId => dispatch => (
+    LikeAPIUtil.deleteLike(imageId)
+        .then(like => dispatch(removeLike(like)))
+);
