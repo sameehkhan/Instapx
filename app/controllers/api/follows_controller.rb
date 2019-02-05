63d393c912046a2a48ac7ac25e7511a6c1c70947
Sameeh <sameehkhan1@gmail.com>
2019-02-05 09:54:04 -0800

    Create controller params

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -1,2 +1,19 @@
 class Api::FollowsController < ApplicationController
+
+    def index
+        
+    end
+
+    def create
+        
+    end
+
+    def destroy
+        
+    end
+
+    def follow_params
+        params.requre(:follow).permit(:user_id, :follower_id)
+    end 
+
 end
