6d0548664ab25acd11bfa310e0fd5135e6197a4d
Sameeh <sameehkhan1@gmail.com>
2019-02-05 10:01:51 -0800

    Pass information for follow show in jbuilder

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -1,9 +1,5 @@
 class Api::FollowsController < ApplicationController
 
-    # def index
-        
-    # end
-
     def create
         @follow = Follow.new(follow_params)
         @follow.follower_id = current_user.id 
@@ -12,13 +8,13 @@ class Api::FollowsController < ApplicationController
     end
 
     def destroy
-        @follow = Follow.wehere(user_id: params[:id]).where(follower_id: current_user.id)[0]
+        @follow = Follow.where(user_id: params[:id]).where(follower_id: current_user.id)[0]
         @follow.destroy
         render :show
     end
 
     def follow_params
-        params.requre(:follow).permit(:user_id, :follower_id)
+        params.requre(:follow).permit(:user_id)
     end 
 
 end
