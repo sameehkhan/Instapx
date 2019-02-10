46230e47aca60dbfccc13c978e7df67ffa85ed8b
Sameeh <sameehkhan1@gmail.com>
2019-02-09 20:24:42 -0800

    Remove references to deleted reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import postsReducer from './posts_reducer';
 import commentsReducer from './comments_reducer';
-import searchReducer from './search_reducer';
 
 const entitiesReducer = combineReducers ({
   users: usersReducer,
   posts: postsReducer,
-  comments: commentsReducer,
-  search: searchReducer
+  comments: commentsReducer
 });
 
 export default entitiesReducer;
